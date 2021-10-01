dac6351131099ac6e1d6305d160def6b4b4cea02
Petya Atanasova <89253024+petyadatanasova@users.noreply.github.com>
2021-10-01 22:43:42 +0300

    Add files via upload

---
+++ C#-Advanced/Streams,FilesAndDirectories-Lab/01.OddLines-StreamReader/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace _01.OddLines_StreamReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("input.txt"))
+            {
+                int counter = 0;
+                string line = reader.ReadLine();
+                using (StreamWriter writer = new StreamWriter("output.txt"))
+                {
+                    while (line != null)
+                    {
+                        if (counter % 2 == 1)
+                        {
+                            Console.WriteLine(line);
+                            writer.WriteLine(line);
+
+                        }
+                        counter++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C#-Advanced/Streams,FilesAndDirectories-Lab/01.OddLines/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace _01.OddLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines(@"../../../input.txt");
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if(i%2==1)
+                {
+                    File.AppendAllText(@"../../../output.txt", $"{lines[i]}{Environment.NewLine}");
+                }
+            }
+        }
+    }
+}
