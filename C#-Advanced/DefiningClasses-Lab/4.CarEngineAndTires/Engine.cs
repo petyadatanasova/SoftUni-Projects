770ea1bcbec79da809c05627b86e975058325425
Petya Atanasova <89253024+petyadatanasova@users.noreply.github.com>
2021-10-10 16:54:43 +0300

    Add files via upload

---
+++ C#-Advanced/DefiningClasses-Lab/4.CarEngineAndTires/Engine.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Engine
+    {
+        public Engine(int horsePower, double cubicCapacity)
+        {
+            HorsePower = horsePower;
+            CubicCapacity = cubicCapacity;
+        }
+        private int horsePower;
+        private double cubicCapacity;
+
+        public int HorsePower
+        {
+            get
+            {
+                return horsePower;
+            }
+            set
+            {
+                horsePower = value;
+            }
+        }
+
+        public double CubicCapacity
+        {
+            get
+            {
+                return cubicCapacity;
+            }
+            set
+            {
+                cubicCapacity = value;
+            }
+        }
+    }
+}
---
+++ C#-Advanced/DefiningClasses-Lab/4.CarEngineAndTires/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string make = Console.ReadLine();
+            string model = Console.ReadLine();
+            int year = int.Parse(Console.ReadLine());
+            double fuelQuantity = double.Parse(Console.ReadLine());
+            double fuelConsumption = double.Parse(Console.ReadLine());
+
+            Car firstCar = new Car();
+            Console.WriteLine(firstCar.WhoAmI());
+            Car secondCar = new Car(make, model, year);
+            Console.WriteLine(secondCar.WhoAmI());
+            Car thirdCar = new Car(make, model, year, fuelQuantity, fuelConsumption);
+            Console.WriteLine(thirdCar.WhoAmI());
+
+            //Tire[] tires = new Tire[4];
+            //tires[0].Year = 1;
+            //tires[1].Year = 1;
+            //tires[2].Year = 2;
+            //tires[3].Year = 2;
+
+            //tires[0].Pressure = 2.5;
+            //tires[1].Pressure = 2.1;
+            //tires[2].Pressure = 0.5;
+            //tires[3].Pressure = 2.3;
+
+            Tire[] tires = new Tire[4]
+            {
+                new Tire(1, 2.5),
+                new Tire(1, 2.1),
+                new Tire(2, 0.5),
+                new Tire(2, 2.3),
+             };
+
+            Engine engine = new Engine(560, 6300);
+
+            Car car = new Car("Lamborgini", "Urus", 2010, 250, 9, engine, tires);
+
+            Console.WriteLine(car.WhoAmI());
+
+        }
+    }
+}
