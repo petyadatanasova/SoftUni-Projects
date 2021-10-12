4c72848332b6a25365b37b6bd0331db9b1f94f97
Petya Atanasova <89253024+petyadatanasova@users.noreply.github.com>
2021-10-12 16:45:06 +0300

    Add files via upload

---
+++ C#-Advanced/DefiningClasses-Exercise/04.OpinionPoll/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                Person person = new Person(name, age);
+
+                people.Add(person);
+            }
+
+            List<Person> filteredPeople = people.Where(x => x.Age > 30).OrderBy(x => x.Name).ToList();
+            foreach (var person in filteredPeople)
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+            
+        }
+    }
+}
