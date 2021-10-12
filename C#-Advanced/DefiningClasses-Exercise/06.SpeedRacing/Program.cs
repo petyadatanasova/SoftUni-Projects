c5c57cfab371235bd011a260054163d2b367edc2
Petya Atanasova <89253024+petyadatanasova@users.noreply.github.com>
2021-10-12 17:55:33 +0300

    Add files via upload

---
+++ C#-Advanced/DefiningClasses-Exercise/06.SpeedRacing/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SpeedRacing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, Car> cars = new Dictionary<string, Car>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string model = input[0];
+                double fuelAmount = double.Parse(input[1]);
+                double fuelConsumption= double.Parse(input[2]);
+
+                Car car = new Car(model,fuelAmount, fuelConsumption);
+                if(!cars.ContainsKey(model))
+                {
+                    cars.Add(model,car);
+                }
+            }
+            string[] command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            while (command[0]!="End")
+            {
+                string carModel = command[1];
+                double amountOfKm = double.Parse(command[2]);
+
+                cars[carModel].CalculateIfCarCanMoveDistance(amountOfKm);
+
+                command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"{car.Key} {car.Value.FuelAmount:f2} {car.Value.TravelledDistance}");
+            }
+        }
+    }
+}
