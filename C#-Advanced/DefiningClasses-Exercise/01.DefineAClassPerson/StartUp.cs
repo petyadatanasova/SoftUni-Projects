165496fe9ad81bfda8fd7f0c2f404162edae8469
Petya Atanasova <89253024+petyadatanasova@users.noreply.github.com>
2021-10-12 14:13:38 +0300

    Add files via upload

---
+++ C#-Advanced/DefiningClasses-Exercise/01.DefineAClassPerson/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            person.Name = "Peter";
+            person.Age = 20;
+
+            Console.WriteLine($"{person.Name} {person.Age}");
+        }
+    }
+}
---
+++ C#-Advanced/DefiningClasses-Exercise/02.CreatingConstructors/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Peter", 20);
+
+            
+           
+
+            Console.WriteLine($"{person.Name} {person.Age}");
+        }
+    }
+}
