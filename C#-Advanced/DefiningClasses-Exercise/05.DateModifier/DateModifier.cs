6aaa1797025e87311b75c84d7085d52df4a237d3
Petya Atanasova <89253024+petyadatanasova@users.noreply.github.com>
2021-10-12 17:22:28 +0300

    Add files via upload

---
+++ C#-Advanced/DefiningClasses-Exercise/05.DateModifier/DateModifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DateModifier
+{
+    public static class DateModifier
+    {
+       
+        public static int GetDaysBetweenTwoDates(string firstDate, string secondDate)
+        {
+            DateTime dateOne = DateTime.Parse(firstDate);
+            DateTime dateTwo = DateTime.Parse(secondDate);
+
+            int days = Math.Abs((dateOne - dateTwo).Days);
+            return days;
+        }
+    }
+}
---
+++ C#-Advanced/DefiningClasses-Exercise/05.DateModifier/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DateModifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+
+            int days = DateModifier.GetDaysBetweenTwoDates(firstDate, secondDate);
+            Console.WriteLine(days);
+        }
+    }
+}
