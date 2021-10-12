f4d0333a3c33991b3a483043b12d8d3f82e06752
Petya Atanasova <89253024+petyadatanasova@users.noreply.github.com>
2021-10-12 14:34:35 +0300

    Add files via upload

---
+++ C#-Advanced/DefiningClasses-Exercise/03.OldestFamilyMember/Family.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        List<Person> people = new List<Person>();
+
+        public void AddMember(Person member)
+        {
+            people.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            Person oldestMember = people.OrderByDescending(x => x.Age).FirstOrDefault();
+            return oldestMember;
+        }
+
+    }
+}
