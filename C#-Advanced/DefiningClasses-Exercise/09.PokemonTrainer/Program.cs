bde95016d23b8dff1a16865c201ed0639bade4f1
Petya Atanasova <89253024+petyadatanasova@users.noreply.github.com>
2021-10-13 15:45:58 +0300

    Add files via upload

---
+++ C#-Advanced/DefiningClasses-Exercise/09.PokemonTrainer/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PokemonTrainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //"{trainerName} {pokemonName} {pokemonElement} {pokemonHealth}"
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, Trainer> trainers = new Dictionary<string, Trainer>();
+            while (input[0] != "Tournament")
+            {
+                string trainerName = input[0];
+                string pokemonName = input[1];
+                string element = input[2];
+                int health = int.Parse(input[3]);
+
+                Pokemon pokemon = new Pokemon(pokemonName, element, health);
+                Trainer trainer = new Trainer(trainerName);
+               // trainer.Pokemons.Add(pokemon);
+
+                if (!trainers.ContainsKey(trainerName))
+                {
+                    trainers.Add(trainerName, trainer);
+                }
+                trainers[trainerName].Pokemons.Add(pokemon);
+
+
+                input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+            string inputElement = Console.ReadLine();
+            while (inputElement != "End")
+            {
+
+                foreach (var trainer in trainers)
+                {
+                    if (trainer.Value.Pokemons.Any(x => x.Element == inputElement))
+                    {
+                        trainer.Value.Badges++;
+                    }
+                    else
+                    {
+                        foreach (var pokemon in trainer.Value.Pokemons)
+                        {
+                            pokemon.Health -= 10;
+                        }
+                        trainer.Value.Pokemons.RemoveAll(x => x.Health <= 0);
+                    }
+                }
+
+
+
+                inputElement = Console.ReadLine();
+            }
+
+            //"{trainerName} {badges} {numberOfPokemon}"
+            foreach (var trainer in trainers.OrderByDescending(x => x.Value.Badges))
+            {
+                Console.WriteLine($"{trainer.Key} {trainer.Value.Badges} {trainer.Value.Pokemons.Count}");
+            }
+
+
+        }
+    }
+}
---
+++ C#-Advanced/DefiningClasses-Exercise/09.PokemonTrainer/Trainer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokemonTrainer
+{
+    public  class Trainer
+    {
+        public Trainer(string name)
+        {
+            Name = name;
+            Pokemons = new List<Pokemon>();
+        }
+        public string  Name { get; set; }
+        public int Badges { get; set; } = 0;
+
+        public List<Pokemon> Pokemons { get; set; }
+    }
+}
