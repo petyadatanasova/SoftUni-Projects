79c824177e800a44a4650837f38486cd0b6f9203
Petya Atanasova <89253024+petyadatanasova@users.noreply.github.com>
2021-10-13 18:04:24 +0300

    Add files via upload

---
+++ C#-Advanced/DefiningClasses-Exercise/10.SoftUniParking/Parking.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniParking
+{
+    public class Parking
+    {
+        public Parking(int capacity)
+        {
+            this.capacity = capacity;
+            this.cars = new Dictionary<string, Car>();
+
+        }
+
+        private Dictionary<string, Car> cars;
+        private int capacity;
+
+        
+
+
+        public string AddCar(Car car)
+        {
+            if(cars.ContainsKey(car.RegistrationNumber))
+            {
+                return "Car with that registration number, already exists!";
+            }
+            else
+            {
+                if(capacity<=0)
+                {
+                    return "Parking is full!";
+                }
+                else
+                {
+                    capacity--;
+                    cars.Add(car.RegistrationNumber, car);
+                   return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+                }
+            }
+        }
+        public string RemoveCar(string RegistrationNumber)
+        {
+            if(!cars.ContainsKey(RegistrationNumber))
+            {
+                return $"Car with that registration number, doesn't exist!";
+            }
+            else
+            {
+                capacity++;
+                cars.Remove(RegistrationNumber);
+                return $"Successfully removed {RegistrationNumber}";
+            }
+        }
+        public Car GetCar(string RegistrationNumber)
+        {
+            return cars[RegistrationNumber];
+        }
+        public void RemoveSetOfRegistrationNumber(List<string> RegistrationNumbers)
+        {
+            foreach (var reg in RegistrationNumbers)
+            {
+                if(cars.ContainsKey(reg))
+                {
+                    cars.Remove(reg);
+                }
+            }
+        }
+        public int Count=>
+       
+             cars.Count;
+        
+    }
+}
