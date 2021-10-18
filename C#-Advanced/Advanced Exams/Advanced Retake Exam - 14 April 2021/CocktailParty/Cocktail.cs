e444cd35bda5692e27f0a9ba2a257a7715f33331
Petya Atanasova <89253024+petyadatanasova@users.noreply.github.com>
2021-10-18 17:38:49 +0300

    Add files via upload

---
+++ C#-Advanced/Advanced Exams/Advanced Retake Exam - 14 April 2021/CocktailParty/Cocktail.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CocktailParty
+{
+    public class Cocktail
+    {
+
+        public Cocktail(string name, int capacity, int maxAlcoholLevel)
+        {
+            Name = name;
+            Capacity = capacity;
+            MaxAlcoholLevel = maxAlcoholLevel;
+            Ingredients = new Dictionary<string, Ingredient>();
+
+        }
+        Dictionary<string, Ingredient> Ingredients;
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int MaxAlcoholLevel { get; set; }
+
+        public void Add(Ingredient ingredient)
+        {
+            if (Ingredients.Count < Capacity && !Ingredients.ContainsKey(ingredient.Name))
+            {
+                Ingredients.Add(ingredient.Name, ingredient);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            if (Ingredients.ContainsKey(name))
+            {
+                return Ingredients.Remove(name);
+            }
+            return false;
+        }
+
+        public Ingredient FindIngredient(string name)
+        {
+            if(Ingredients.ContainsKey(name))
+            {
+                return Ingredients[name];
+            }
+            return null;
+        }
+
+        public Ingredient GetMostAlcoholicIngredient()
+        {
+            if (Ingredients.Any())
+            {
+                var mostAlcohol = Ingredients.Select(x => x.Value.Alcohol).Max();
+                var ingredient = Ingredients.FirstOrDefault(x => x.Value.Alcohol == mostAlcohol);
+                return ingredient.Value;
+            }
+            return null;
+        }
+        public int CurrentAlcoholLevel => Ingredients.Select(x => x.Value.Alcohol).Sum();
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Cocktail: {Name} - Current Alcohol Level: {CurrentAlcoholLevel}");
+            foreach (var item in Ingredients)
+            {
+                sb.AppendLine(item.Value.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
