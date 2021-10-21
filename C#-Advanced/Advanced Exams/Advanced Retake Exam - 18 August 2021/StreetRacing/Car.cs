87a8116b5585717d6ddb90ac4cf468e7bce34d5d
Petya Atanasova <89253024+petyadatanasova@users.noreply.github.com>
2021-10-21 19:56:45 +0300

    Add files via upload

---
+++ C#-Advanced/Advanced Exams/Advanced Retake Exam - 18 August 2021/StreetRacing/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StreetRacing
+{
+    public class Car
+    {
+        public Car(string make, string model, string licencePlate, int horsePower, double weight)
+        {
+            Make = make;
+            Model = model;
+            LicensePlate = licencePlate;
+            HorsePower = horsePower;
+            Weight = weight;
+        }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string LicensePlate { get; set; }
+        public int HorsePower { get; set; }
+        public double Weight { get; set; }
+
+        public override string ToString()
+        {
+            return @$"Make: {Make}
+Model: {Model}
+License Plate: {LicensePlate}
+Horse Power: {HorsePower}
+Weight: {Weight}";
+
+        }
+
+    }
+}
+
