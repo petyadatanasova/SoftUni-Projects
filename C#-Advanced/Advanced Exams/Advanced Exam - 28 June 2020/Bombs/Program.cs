f7705cae9273bf5e40c212c024028acfd6e2f4b7
Petya Atanasova <89253024+petyadatanasova@users.noreply.github.com>
2021-10-21 14:57:46 +0300

    Add files via upload

---
+++ C#-Advanced/Advanced Exams/Advanced Exam - 28 June 2020/Bombs/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bombs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> bombEffects = new Queue<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            Stack<int> bombCasings = new Stack<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            SortedDictionary<string, int> bombPouch = new SortedDictionary<string, int>();
+            bombPouch.Add("Datura Bombs", 0);
+            bombPouch.Add("Cherry Bombs", 0);
+            bombPouch.Add("Smoke Decoy Bombs", 0);
+
+            while (bombEffects.Any() && bombCasings.Any())
+            {
+                int effect = bombEffects.Peek();
+                int casing = bombCasings.Pop();
+
+                if(effect+casing==40 || effect + casing == 60 || effect + casing == 120)
+                {
+                    bombEffects.Dequeue();
+                    if(effect + casing == 40)
+                    {
+                       
+                        bombPouch["Datura Bombs"]++;
+                    }
+                    else if (effect + casing == 60)
+                    {
+                       
+                        bombPouch["Cherry Bombs"]++;
+                    }
+                    else if (effect + casing == 120)
+                    {
+                      
+                        bombPouch["Smoke Decoy Bombs"]++;
+                    }
+                }
+                else
+                {
+                    casing -= 5;
+                    bombCasings.Push(casing);
+                }
+                if(bombPouch["Datura Bombs"] >= 3 && bombPouch["Cherry Bombs"] >= 3 & bombPouch["Smoke Decoy Bombs"] >= 3)
+                {
+                    break;
+                }
+
+            }
+
+            if(bombPouch["Datura Bombs"]>=3 && bombPouch["Cherry Bombs"]>= 3 & bombPouch["Smoke Decoy Bombs"]>= 3)
+            {
+                Console.WriteLine("Bene! You have successfully filled the bomb pouch!");
+            }
+            else
+            {
+                Console.WriteLine("You don't have enough materials to fill the bomb pouch.");
+            }
+            if(bombEffects.Any())
+            {
+                Console.WriteLine($"Bomb Effects: {string.Join(", ", bombEffects)}");
+            }
+            else
+            {
+                Console.WriteLine("Bomb Effects: empty");
+            }
+            if (bombCasings.Any())
+            {
+                Console.WriteLine($"Bomb Casings: {string.Join(", ", bombCasings)}");
+            }
+            else
+            {
+                Console.WriteLine("Bomb Casings: empty");
+            }
+
+            foreach (var bomb in bombPouch)
+            {
+                Console.WriteLine($"{bomb.Key}: {bomb.Value}");
+            }
+        }
+    }
+}
