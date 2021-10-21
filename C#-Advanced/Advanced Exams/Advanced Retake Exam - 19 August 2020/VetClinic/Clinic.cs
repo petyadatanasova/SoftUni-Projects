42d378ff1314982f73e3b6a5f922f9e0b73e8ec0
Petya Atanasova <89253024+petyadatanasova@users.noreply.github.com>
2021-10-21 14:22:20 +0300

    Add files via upload

---
+++ C#-Advanced/Advanced Exams/Advanced Retake Exam - 19 August 2020/VetClinic/Clinic.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VetClinic
+{
+   public class Clinic
+    {
+        public Clinic(int capacity)
+        {
+            Capacity = capacity;
+            Pets = new List<Pet>();
+        }
+
+        public List<Pet> Pets { get; set; }
+        public int Capacity { get; set; }
+
+        public int Count => Pets.Count;
+        public void Add(Pet pet)
+        {
+            if(Pets.Count<Capacity)
+            {
+                Pets.Add(pet);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Pet pet = Pets.FirstOrDefault(x => x.Name == name);
+            if(pet!=null)
+            {
+                Pets.Remove(pet);
+                return true;
+            }
+            return false;
+        }
+        public Pet GetPet(string name, string owner)
+        {
+            Pet pet = Pets.FirstOrDefault(x => x.Name == name && x.Owner==owner);
+            if (pet != null)
+            {
+                return pet;
+            }
+            return null;
+
+        }
+
+        public Pet GetOldestPet()
+        {
+            int oldestAge = Pets.Select(x => x.Age).Max();
+            Pet pet = Pets.FirstOrDefault(x => x.Age == oldestAge);
+            if(pet!=null)
+            {
+                return pet;
+            }
+            return null;
+        }
+
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("The clinic has the following patients:");
+            foreach (var pet in Pets)
+            {
+                sb.AppendLine($"Pet {pet.Name} with owner: {pet.Owner}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+
+    }
+}
