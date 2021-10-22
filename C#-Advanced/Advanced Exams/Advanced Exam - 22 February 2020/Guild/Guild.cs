09d7d0a04cc3d3e549065b535047135a2dd5c4f6
Petya Atanasova <89253024+petyadatanasova@users.noreply.github.com>
2021-10-22 18:42:41 +0300

    Add files via upload

---
+++ C#-Advanced/Advanced Exams/Advanced Exam - 22 February 2020/Guild/Guild.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Guild
+{
+   public class Guild
+    {
+      public Guild(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            roster = new List<Player>();
+
+        }
+        public List<Player> roster { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Count => roster.Count;
+
+        public void AddPlayer(Player player)
+        {
+            if(roster.Count<Capacity)
+            {
+                roster.Add(player);
+            }
+        }
+        public bool RemovePlayer(string name)
+        {
+            Player player = roster.FirstOrDefault(x => x.Name == name);
+            if(player!=null)
+            {
+                roster.Remove(player);
+                return true;
+            }
+            return false;
+        }
+
+        public void PromotePlayer(string name)
+        {
+            Player player = roster.FirstOrDefault(x => x.Name == name);
+            if(player!=null && player.Rank=="Trial")
+            {
+                player.Rank = "Member";
+            }
+
+        }
+        public void DemotePlayer(string name)
+        {
+            Player player = roster.FirstOrDefault(x => x.Name == name);
+            if (player != null && player.Rank != "Trial")
+            {
+                player.Rank = "Trial";
+            }
+        }
+
+        public Player[] KickPlayersByClass(string @class)
+        {
+            List<Player> kickedPlayers = roster.Where(x => x.Class == @class).ToList();
+
+            if(kickedPlayers.Count>0)
+            {
+                roster.RemoveAll(x => x.Class == @class);
+            }
+            return kickedPlayers.ToArray();
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Players in the guild: {Name}");
+            foreach (var item in roster)
+            {
+                sb.AppendLine(item.ToString().TrimEnd());
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+
+
+    }
+}
---
+++ C#-Advanced/Advanced Exams/Advanced Exam - 22 February 2020/LootBox/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LootBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> firstBox = new Queue<int>();
+            Stack<int> secondBox = new Stack<int>();
+            int[] firstInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] secondInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int collection = 0;
+
+            for (int i = 0; i < firstInput.Length; i++)
+            {
+                firstBox.Enqueue(firstInput[i]);
+            }
+            for (int i = 0; i < secondInput.Length; i++)
+            {
+                secondBox.Push(secondInput[i]);
+            }
+
+            while (firstBox.Any() && secondBox.Any())
+            {
+                int firstItem = firstBox.Peek();
+                int secondItem = secondBox.Pop();
+
+                int sum = firstItem + secondItem;
+                if(sum%2==0)
+                {
+                    firstBox.Dequeue();
+                    collection += sum;
+
+                }
+                else
+                {
+                    firstBox.Enqueue(secondItem);
+                }
+            }
+            if(firstBox.Count==0)
+            {
+                Console.WriteLine("First lootbox is empty");
+            }
+            else if (secondBox.Count==0)
+            {
+                Console.WriteLine("Second lootbox is empty");
+            }
+
+            if(collection>=100)
+            {
+                Console.WriteLine($"Your loot was epic! Value: {collection}");
+            }
+            else
+            {
+                Console.WriteLine($"Your loot was poor... Value: {collection}");
+            }
+        }
+    }
+}
