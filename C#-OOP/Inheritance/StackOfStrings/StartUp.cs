3ae69ceae470eddd6ffdadba54b544863f24b9e2
Petya Atanasova <89253024+petyadatanasova@users.noreply.github.com>
2021-10-31 10:49:35 +0200

    Add files via upload

---
+++ C#-OOP/Inheritance/StackOfStrings/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings str = new StackOfStrings();
+
+            str.Push("1");
+
+            Console.WriteLine(str.isEmpty());
+            str.Pop();
+            Console.WriteLine(str.isEmpty());
+            str.Push("1");
+            Stack<string> list = new Stack<string>();
+
+            list.Push("one");
+            list.Push("two");
+            list.Push("three");
+
+            str.AddRange(list);
+            foreach (var st in str)
+            {
+                Console.WriteLine(st);
+            }
+
+        }
+
+    }
+}
