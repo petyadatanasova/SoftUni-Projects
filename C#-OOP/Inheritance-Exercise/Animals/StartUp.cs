6c1f9f64a60754e3cbff4b77b6bb701bde3bdd57
Petya Atanasova <89253024+petyadatanasova@users.noreply.github.com>
2021-11-01 17:50:57 +0200

    Add files via upload

---
+++ C#-OOP/Inheritance-Exercise/Animals/StartUp.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            List<Animal> animals = new List<Animal>();
+            while (type != "Beast!")
+            {
+                string[] input = Console.ReadLine().Split();
+                if (input.Length != 3 || int.Parse(input[1]) < 0)
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+                else
+                {
+                    string name = input[0];
+                    int age = int.Parse(input[1]);
+                    string gender = input[2];
+
+                    if (type == "Dog")
+                    {
+                        Dog dog = new Dog(name, age, gender);
+                        animals.Add(dog);
+                    }
+                    else if (type == "Cat")
+                    {
+                        Cat cat = new Cat(name, age, gender);
+                        animals.Add(cat);
+                    }
+                    else if (type == "Frog")
+                    {
+                        Frog frog = new Frog(name, age, gender);
+                        animals.Add(frog);
+                    }
+                    else if (type == "Kitten")
+                    {
+                        Kitten kitten = new Kitten(name, age);
+                        animals.Add(kitten);
+                    }
+                    else if (type == "Tomcat")
+                    {
+                        Tomcat tomcat = new Tomcat(name, age);
+                        animals.Add(tomcat);
+                    }
+
+                }
+
+                type = Console.ReadLine();
+            }
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal.GetType().Name);
+                Console.WriteLine($"{animal.Name} {animal.Age} {animal.Gender}");
+                Console.WriteLine(animal.ProduceSound());
+            }
+
+        }
+    }
+}
---
+++ C#-OOP/Inheritance-Exercise/Person/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Person
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            
+
+            if(age<=15 && age>0)
+            {
+                Child child = new Child(name, age);
+                Console.WriteLine(child);
+            }
+            else if (age>15)
+            {
+               Person person = new Person(name, age);
+                Console.WriteLine(person);
+            }
+            
+          
+
+        }
+    }
+}
