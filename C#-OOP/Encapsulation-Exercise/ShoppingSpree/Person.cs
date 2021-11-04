e8ac44d83f764efdf2a34b014d0614885062ca88
Petya Atanasova <89253024+petyadatanasova@users.noreply.github.com>
2021-11-04 19:11:50 +0200

    Add files via upload

---
+++ C#-OOP/Encapsulation-Exercise/ShoppingSpree/Person.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShoppingSpree
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private List<Product> products;
+
+        public Person(string name, decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+            products = new List<Product>();
+        }
+
+        public string Name
+        {
+            get { return name; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                name = value;
+            }
+        }
+
+        public decimal Money
+        {
+            get { return money; }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                money = value;
+            }
+        }
+        public void AddProduct(Product product)
+        {
+            if(money<product.Cost)
+            {
+                throw new ArgumentException($"{Name} can't afford {product.Name}");
+                
+            }
+            products.Add(product);
+            money -= product.Cost;
+            Console.WriteLine($"{Name} bought {product.Name}");
+            
+        }
+        public override string ToString()
+        {
+            return $"{Name} - {(products.Any() ? string.Join(", ", products) : "Nothing bought")}";
+        }
+
+    }
+}
---
+++ C#-OOP/Encapsulation-Exercise/ShoppingSpree/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShoppingSpree
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] peopleInput = Console.ReadLine().Split(";");
+            Dictionary<string, Person> people = new Dictionary<string, Person>();
+            try
+            {
+                for (int i = 0; i < peopleInput.Length; i++)
+                {
+                    string[] peopleInfo = peopleInput[i].Split("=", StringSplitOptions.RemoveEmptyEntries);
+
+                    string name = peopleInfo[0];
+                    decimal money = decimal.Parse(peopleInfo[1]);
+                    Person person = new Person(name, money);
+                    people.Add(name, person);
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            string[] productsInput = Console.ReadLine().Split(";", StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, Product> products = new Dictionary<string, Product>();
+            try
+            {
+                for (int i = 0; i < productsInput.Length; i++)
+                {
+                    string[] productsInfo = productsInput[i].Split("=");
+                    string nameProduct = productsInfo[0];
+                    decimal cost = decimal.Parse(productsInfo[1]);
+                    Product product = new Product(nameProduct, cost);
+                    products.Add(nameProduct, product);
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                return;
+                
+            }
+
+            string [] commandInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            while (commandInput[0]!="END")
+            {
+                string namePerson = commandInput[0];
+                string nameProductToBuy = commandInput[1];
+
+                if(people.ContainsKey(namePerson))
+                {
+                    try
+                    {
+                        people[namePerson].AddProduct(products[nameProductToBuy]);
+                    }
+                    catch (Exception ex)
+                    {
+
+                        Console.WriteLine(ex.Message);
+                       
+                    }
+                }
+
+                commandInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+            foreach (var person in people)
+            {
+                Console.WriteLine(person.Value);
+                
+            }
+
+        }
+    }
+}
