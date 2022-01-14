f1001a454f29726a5281aa51b5b36e083520bcdd
Petya Atanasova <89253024+petyadatanasova@users.noreply.github.com>
2022-01-14 17:51:16 +0200

    Add files via upload

---
+++ C#-OOP/Inheritance/StackOfStrings/StackOfStringsUpdated/StackOfStrings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    //public class StackOfStrings <T> : Stack<T>
+    {
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+        public Stack<string> AddRange(List<string> list)
+        {
+            foreach (var item in list)
+            {
+                this.Push(item);
+            }
+            
+            return this;
+        }
+    }
+}
---
+++ C#-OOP/Inheritance/StackOfStrings/StackOfStringsUpdated/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stack = new StackOfStrings();
+            List<string> list = new List<string>();
+
+            list.Add("1");
+            list.Add("2");
+            list.Add("3");
+            list.Add("4");
+            list.Add("5");
+
+
+            Console.WriteLine($"{string.Join(" ", stack)}");
+
+            Console.WriteLine(stack.IsEmpty()); 
+            stack.AddRange(list);
+            Console.WriteLine(stack.IsEmpty());
+            Console.WriteLine($"{string.Join(" ", stack)}");
+        }
+    }
+}
