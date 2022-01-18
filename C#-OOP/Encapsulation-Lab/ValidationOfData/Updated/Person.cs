f9d2b7d0d7c0b1aab2f7b846a30ba457e3243820
Petya Atanasova <89253024+petyadatanasova@users.noreply.github.com>
2022-01-18 10:57:04 +0200

    Add files via upload

---
+++ C#-OOP/Encapsulation-Lab/ValidationOfData/Updated/Person.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public string FirstName
+        {
+            get=> this.firstName;
+            private set
+            { 
+            if(value.Length<3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+                this.firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get => this.lastName;
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+                this.lastName = value;
+            }
+        }
+        public int Age 
+        {
+            get => this.age;
+            private set
+            {
+                if(value<=0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+                this.age = value;
+            }
+        }
+        public decimal Salary
+        {
+            get => this.salary;
+            private set
+            { 
+                if(value<650)
+                {
+                    throw new ArgumentException("Salary cannot be less than 650 leva!");
+                }
+                this.salary = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.firstName} {this.lastName} receives {this.salary:f2} leva.";
+        } 
+        public void IncreaseSalary(decimal percentage)
+        {
+            if(this.age<30)
+            {
+                percentage /= 2;
+            }
+            this.salary += this.salary * percentage/100;
+        }
+    }
+}
