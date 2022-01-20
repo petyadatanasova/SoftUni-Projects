3ced154500a8c4271e601aa37b19a2b7d1be8a20
Petya Atanasova <89253024+petyadatanasova@users.noreply.github.com>
2022-01-20 11:45:40 +0200

    Add files via upload

---
+++ C#-OOP/InterfacesAndAbstraction-Lab/Cars/Seat.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.Color} {nameof(Seat)} {this.Model}");
+            sb.AppendLine(Start());
+            sb.Append(Stop());
+
+            return sb.ToString();
+        }
+    }
+}
