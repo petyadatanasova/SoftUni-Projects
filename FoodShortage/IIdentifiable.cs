2081d07f23b33ceaa871b4c6186c4092e578b0c2
Petya Atanasova <89253024+petyadatanasova@users.noreply.github.com>
2022-01-21 16:13:06 +0200

    Add files via upload

---
+++ FoodShortage/IIdentifiable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BirthdayCelebrations
+{
+    public interface IIdentifiable
+    {
+        public string Id { get; set; }
+    }
+}
