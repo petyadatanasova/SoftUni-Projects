a157373f616e6a57f692836be09625a3506b5181
Petya Atanasova <89253024+petyadatanasova@users.noreply.github.com>
2022-01-22 12:37:14 +0200

    Add files via upload

--- C#-OOP/InterfacesAndAbstraction-Exercise/Military Elite/LieutenantGeneral.cs
+++ C#-OOP/InterfacesAndAbstraction-Exercise/Military Elite/LieutenantGeneral.cs
@@ -4,15 +4,15 @@ using System.Text;
 
 namespace MilitaryElite
 {
-    public class LieutenantGeneral : Private
+    public class LieutenantGeneral : Private, ILieutenantGeneral
     {
         public LieutenantGeneral(string firstName, string lastName, int id, decimal salary) : base(firstName, lastName, id, salary)
         {
-            Privates = new List<Private>();
+            Privates = new List<IPrivate>();
         }
         //public List<Private> myPrivates { get; set; }
 
-        public List<Private> Privates { get; set; }
+        public List<IPrivate> Privates { get; set; }
         
 
         public override string ToString()
