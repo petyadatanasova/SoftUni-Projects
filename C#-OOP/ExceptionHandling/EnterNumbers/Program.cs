7b713b63a8044e03e12d785a579b8005f08de2a8
Petya Atanasova <89253024+petyadatanasova@users.noreply.github.com>
2022-02-09 17:24:33 +0200

    Add files via upload

---
+++ C#-OOP/ExceptionHandling/EnterNumbers/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EnterNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<int> numbers = new List<int>();
+              int startRange = 1;
+              int endRange = 100;
+            while (numbers.Count<10)
+            {
+                try
+                {
+                    ReadNumber(startRange, endRange, numbers);
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+                if (numbers.Count > 0)
+                {
+                    startRange = numbers[numbers.Count - 1];
+                }
+            }
+
+            Console.WriteLine($"{string.Join(", ", numbers)}");
+        }
+        public static void ReadNumber(int start, int end, List<int>numbers)
+        {
+            string input = Console.ReadLine();
+
+            if(!int.TryParse(input, out int number))
+            {
+                throw new ArgumentException("Invalid Number!");
+            }
+            else if (int.Parse(input)<=start || int.Parse(input)>=end)
+            {
+                throw new ArgumentException($"Your number is not in range {start} - 100!");
+            }
+            numbers.Add(int.Parse(input));
+            
+        }
+    }
+}
