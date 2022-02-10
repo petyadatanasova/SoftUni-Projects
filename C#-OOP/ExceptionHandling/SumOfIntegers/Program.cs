56a6da49478a8185c392e90d6aa0df82659eae86
Petya Atanasova <89253024+petyadatanasova@users.noreply.github.com>
2022-02-10 09:15:34 +0200

    Add files via upload

---
+++ C#-OOP/ExceptionHandling/SumOfIntegers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SumOfIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int totalSum = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                try
+                {
+                    if (int.TryParse(input[i], out int integerValue))
+                    {
+                       
+                        totalSum += integerValue;
+                    }
+                    else
+                    {
+                        if (long.TryParse(input[i],out long longIntegerValue))
+                        {
+                            if (longIntegerValue > int.MaxValue || longIntegerValue<int.MaxValue)
+                            {
+                                throw new IndexOutOfRangeException($"The element '{input[i]}' is out of range!");
+                            }
+                        }
+                        throw new FormatException($"The element '{input[i]}' is in wrong format!");
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }        
+                Console.WriteLine($"Element '{input[i]}' processed - current sum: {totalSum}");
+            }
+            Console.WriteLine($"The total sum of all integers is: {totalSum}");
+        }
+    }
+}
