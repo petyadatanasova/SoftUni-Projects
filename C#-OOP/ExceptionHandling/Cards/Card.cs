0f945f1b093f47158ff0427b140d6a330be0bb3e
Petya Atanasova <89253024+petyadatanasova@users.noreply.github.com>
2022-02-10 09:00:51 +0200

    Add files via upload

---
+++ C#-OOP/ExceptionHandling/Cards/Card.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cards
+{
+    public class Card
+    {
+        public Card(string face, string suit)
+        {
+            Face = face;
+            Suit = suit;
+        }
+
+        public string Face { get; private set; }
+        public string Suit { get; private set; }
+
+        public override string ToString()
+        {
+            char suitForPrint;
+            switch (Suit)
+            {
+                case "S":
+                    suitForPrint = '\u2660';
+                    break;
+                case "H":
+                    suitForPrint = '\u2665';
+                    break;
+                case "D":
+                    suitForPrint = '\u2666';
+                    break;
+                case "C":
+                    suitForPrint = '\u2663';
+                    break;
+                default:
+                    suitForPrint =' ';
+                    break;
+            }
+            return $"[{Face}{suitForPrint}]";
+        }
+        //S = '\u2660',
+        //H = '\u2665',
+        //D = '\u2666',
+        //C = '\u2663'
+    }
+}
---
+++ C#-OOP/ExceptionHandling/Cards/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cards
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+     
+
+            Console.OutputEncoding = Encoding.UTF8;
+            List<Card> cards = new List<Card>();
+         
+            string[] input = Console.ReadLine().Split(", ");
+            //
+            //A S, 10 D, K H, 2 C
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                try
+                {
+                    string[] cardValues = input[i].Split();
+
+                    string face = cardValues[0];
+                    string suit = cardValues[1];
+
+
+                    if ((face == "2" || face == "3" || face == "4" || face == "5" || face == "6" || face == "7" || face == "8" ||
+                    face == "9" || face == "10" || face == "J" || face == "K" || face == "Q" || face == "A") && (suit == "S" ||
+                    suit == "D" || suit == "H" || suit == "C"))
+                    {
+                        Card card = new Card(face, suit);
+                        cards.Add(card);
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Invalid card!");
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+            Console.WriteLine($"{string.Join(" ", cards)}");
+        }
+
+    }
+}
