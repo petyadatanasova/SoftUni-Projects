025932d1fcaff2b717b65516a2d81c86045748b9
Petya Atanasova <89253024+petyadatanasova@users.noreply.github.com>
2022-02-14 23:51:53 +0200

    Add files via upload

---
+++ CodeTracker/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Victor")]
+    class StartUp
+    {
+        [Author("George")]
+        [Author("Victor")]
+        static void Main(string[] args)
+        {
+
+            Tracker tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+        }
+    }
+}
