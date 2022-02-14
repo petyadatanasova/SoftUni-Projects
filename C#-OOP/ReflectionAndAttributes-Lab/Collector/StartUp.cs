aecf2c5ea3e93d54a99e786afce66252fb414287
Petya Atanasova <89253024+petyadatanasova@users.noreply.github.com>
2022-02-14 23:19:58 +0200

    Add files via upload

---
+++ C#-OOP/ReflectionAndAttributes-Lab/Collector/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Stealer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            string result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+            Console.WriteLine(result);
+
+            Console.WriteLine();
+            Console.WriteLine(new string ('*', 60 ));
+            Console.WriteLine();
+
+            Spy spy1 = new Spy();
+            string result1 = spy.AnalyzeAccessModifiers("Stealer.Hacker");
+            Console.WriteLine(result1);
+
+            Console.WriteLine();
+            Console.WriteLine(new string('*', 60));
+            Console.WriteLine();
+
+            Spy spy2 = new Spy();
+            string result2 = spy.RevealPrivateMethods("Stealer.Hacker");
+            Console.WriteLine(result2);
+
+            Console.WriteLine();
+            Console.WriteLine(new string('*', 60));
+            Console.WriteLine();
+
+            Spy spy3 = new Spy();
+            string result3 = spy.CollectGettersAndSetters("Stealer.Hacker");
+            Console.WriteLine(result3);
+        }
+    }
+}
