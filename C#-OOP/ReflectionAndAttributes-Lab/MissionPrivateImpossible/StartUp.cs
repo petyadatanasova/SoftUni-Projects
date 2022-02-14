c43a649a78cce1aca2b45b15abc5d5bc2caef985
Petya Atanasova <89253024+petyadatanasova@users.noreply.github.com>
2022-02-14 18:16:54 +0200

    Add files via upload

---
+++ C#-OOP/ReflectionAndAttributes-Lab/MissionPrivateImpossible/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Stealer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            string result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+            Console.WriteLine(result);
+
+            Console.WriteLine();
+            Console.WriteLine(new string ('*', 60 ));
+            Console.WriteLine();
+
+            Spy spy1 = new Spy();
+            string result1 = spy.AnalyzeAccessModifiers("Stealer.Hacker");
+            Console.WriteLine(result1);
+
+            Console.WriteLine();
+            Console.WriteLine(new string('*', 60));
+            Console.WriteLine();
+
+            Spy spy2 = new Spy();
+            string result2 = spy.RevealPrivateMethods("Stealer.Hacker");
+            Console.WriteLine(result2);
+
+            Console.WriteLine();
+            Console.WriteLine(new string('*', 60));
+            Console.WriteLine();
+
+        }
+    }
+}
