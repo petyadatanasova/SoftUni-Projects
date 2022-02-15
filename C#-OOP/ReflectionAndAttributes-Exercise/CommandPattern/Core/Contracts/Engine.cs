26c6a27c402aad2709e0135566cb3ceb4baca718
Petya Atanasova <89253024+petyadatanasova@users.noreply.github.com>
2022-02-15 17:19:42 +0200

    Add files via upload

---
+++ C#-OOP/ReflectionAndAttributes-Exercise/CommandPattern/Core/Contracts/Engine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommandPattern.Core.Contracts
+{
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter commandInterpreter;
+
+        public Engine(ICommandInterpreter commandInterpreter)
+        {
+            this.commandInterpreter = commandInterpreter;
+        }
+
+        public void Run()
+        {
+            string engineCondition;
+            while (true)
+            {
+                engineCondition = commandInterpreter.Read(Console.ReadLine());
+                if(engineCondition==null)
+                {
+                    break;
+                }
+                Console.WriteLine(engineCondition);
+            }
+        }
+    }
+}
---
+++ C#-OOP/ReflectionAndAttributes-Exercise/ValidationAttributes/MyRangeAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidationAttributes
+{
+    public class MyRangeAttribute : MyValidationAttribute
+    {
+        private int minValue;
+        private int maxValue;
+
+        public MyRangeAttribute(int minValue, int maxValue)
+        {
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+        }
+        
+        public override bool IsValid(object obj)
+        {
+            if ((int)obj >= minValue && (int)obj <= maxValue)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
